52d343ed93e3693d721d49728eb8a7364774b7e3
tsaristbomba <andrexerdna@gmail.com>
2021-01-26 08:28:24 -0300

    netlify cms limit testing

---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const FooterContainer = styled.div``
+export const FooterWrapper = styled.div``
+export const FooterContent = styled.div``
+export const ColumnOne = styled.div``
+export const FooterSocials = styled.div``
+export const SocialLink = styled.a``
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,19 @@ import {
   AiFillTwitterSquare,
 } from "react-icons/ai"
 
-// import data from "../../../content/footer.yml"
+import data from "../../../content/footer.yml"
+
+import {
+  FooterContainer,
+  FooterWrapper,
+  FooterContent,
+  ColumnOne,
+  FooterSocials,
+  SocialLink,
+} from "./Footer.styles"
 
 const Footer = () => {
-  //   console.log(data)
+  console.log(data)
 
   function getSocialIcon(social) {
     const lower = social.toLowerCase()
@@ -24,33 +33,32 @@ const Footer = () => {
       case "twitter":
         return <AiFillTwitterSquare />
       default:
-        return ""
+        break
     }
   }
 
   return (
-    // <FooterContainer>
-    //   <FooterWrapper>
-    //     <FooterContent>
-    //       <ColumnOne>
-    //         <h2>{data.columnOne.list}</h2>
-    //         <ul>
-    //           {data.columnOne.items.map((item, key) => (
-    //             <li key={key}>{item}</li>
-    //           ))}
-    //         </ul>
-    //       </ColumnOne>
-    //     </FooterContent>
-    //     <FooterSocials>
-    //       {data.socials.map((item, key) => (
-    //         <SocialLink key={key} href={item.link} target="__blank">
-    //           {getSocialIcon(item.socialNetwork)}
-    //         </SocialLink>
-    //       ))}
-    //     </FooterSocials>
-    //   </FooterWrapper>
-    // </FooterContainer>
-    <div></div>
+    <FooterContainer>
+      <FooterWrapper>
+        <FooterContent>
+          <ColumnOne>
+            {/* <h2>{data.columnOne.list}</h2>
+            <ul>
+              {data.columnOne.items.map((item, key) => (
+                <li key={key}>{item}</li>
+              ))}
+            </ul> */}
+          </ColumnOne>
+        </FooterContent>
+        <FooterSocials>
+          {data.socials.map((item, key) => (
+            <SocialLink key={key} href={item.link} target="__blank">
+              {getSocialIcon(item.socialNetwork)}
+            </SocialLink>
+          ))}
+        </FooterSocials>
+      </FooterWrapper>
+    </FooterContainer>
   )
 }
 
